d14ca6e152e4cb6a2f943277b7e2344a82523c80
Oleksandr Tytyk <aleks.tit3@gmail.com>
2024-03-17 19:48:06 +0200

    Done

--- src/Components/ErrorMessage/ErrorMessage.jsx
+++ src/Components/ErrorMessage/ErrorMessage.jsx
@@ -3,7 +3,7 @@ import css from "./ErrorMessage.module.css";
 const ErrorMessage = () => {
   return (
     <div className={css.errorMsg}>
-      <p>Whoops, something went wrong! Please try reloading this page!</p>
+      <p>Whoops, something went wrong! Please try reloading this page later!</p>
     </div>
   );
 };
